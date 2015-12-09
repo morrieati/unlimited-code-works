06eeed2a72489c166501c2bd77f9a49a00802e7e
morrieati <morrislew@outlook.com>
2015-12-09 13:24:48 +0800

    New content

---
+++ CSharp Fundamentals/Decisions/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decisions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Please type something and press the Enter key.");
+            //string userValue;
+            //userValue = Console.ReadLine();
+            //Console.WriteLine("You typed:" + userValue);
+
+            Console.WriteLine("Would you prefer what is behind dorr number 1, 2, or 3?");
+            string userValue = Console.ReadLine();
+
+            //string message = "";
+
+            //if (userValue == "1")
+            //{
+            //    message = "You won a new car!";
+            //}
+            //else if (userValue == "2")
+            //{
+            //    message = "You won a new boat!";
+            //}
+            //else if (userValue == "3")
+            //{
+            //    message = "You won a new cat!";
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Sorry, we did't understand. You lose!");
+            //}
+            //Console.WriteLine(message);
+
+            string message = (userValue == "1") ? "boat" : "strand of lint";
+            Console.WriteLine("You won a {0}", message);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharp Fundamentals/Variables/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int x;
+            //int y;
+            //x = 7;
+            //y = x + 3;
+            //Console.WriteLine(y);
+
+            //string myFirstName;
+            //myFirstName = "Bob";
+            //Console.WriteLine(myFirstName);
+
+            //var myLastName = "Tabor";
+            //Console.WriteLine(myLastName);
+
+            int x = 7;
+            string y = "Bob";
+            string myFirstTry = x + y;
+            Console.WriteLine(myFirstTry);
+
+            Console.ReadLine();
+
+        }
+    }
+}
