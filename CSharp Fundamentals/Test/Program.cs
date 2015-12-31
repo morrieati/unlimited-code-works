8bb777e65b7efd3a4e79aa6b044be6a0c8680b2a
morrieati <morrislew@outlook.com>
2016-01-01 02:07:41 +0800

    Classes: Inheritance, abstract

---
+++ CSharp Fundamentals/Test/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Test
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+			Console.ReadLine();
+		}
+	}
+}
