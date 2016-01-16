33a1e0bfda5c914d7b018ce68dd889d71ae8538a
morrieati <morrislew@outlook.com>
2016-01-16 23:02:44 +0800

    226. Invert Binary Tree

---
+++ LeetCodeCSharp/226_InvertBinaryTree/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _226_InvertBinaryTree
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+
+		public TreeNode InvertTree(TreeNode root)
+		{
+			if (root == null)
+			{
+				return null;
+			}
+
+			TreeNode temp;
+			temp = root.left;
+			root.left = InvertTree(root.right);
+			root.right = InvertTree(temp);
+			
+			return root;
+		}
+	}
+}
---
+++ LeetCodeCSharp/226_InvertBinaryTree/TreeNode.cs
@@ -0,0 +1,13 @@
+﻿namespace _226_InvertBinaryTree
+{
+	public class TreeNode
+	{
+		public int val;
+		public TreeNode left;
+		public TreeNode right;
+		public TreeNode(int x)
+		{
+			val = x;
+		}
+	}
+}
