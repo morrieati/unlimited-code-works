51fb1f66300a2d4b8bc862b55943f7e0870de081
morrieati <morrislew@outlook.com>
2016-01-16 20:53:11 +0800

    104. Maximum Depth of Binary Tree

---
+++ LeetCodeCSharp/104_MaximumDepthOfBinaryTree/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _104_MaximumDepthOfBinaryTree
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+
+		public int MaxDepth(TreeNode root)
+		{
+			int leftDepth, rightDepth;
+			if (root == null)
+			{
+				return 0;
+			}
+			else
+			{
+				leftDepth = MaxDepth(root.left);
+				rightDepth = MaxDepth(root.right);
+			}
+
+			if (leftDepth > rightDepth)
+			{
+				root.val = leftDepth + 1;
+			}
+			else
+			{
+				root.val = rightDepth + 1;
+			}
+
+			return root.val;
+		}
+	}
+}
---
+++ LeetCodeCSharp/104_MaximumDepthOfBinaryTree/TreeNode.cs
@@ -0,0 +1,13 @@
+﻿namespace _104_MaximumDepthOfBinaryTree
+{
+	public class TreeNode
+	{
+		public int val;
+		public TreeNode left;
+		public TreeNode right;
+		public TreeNode(int x)
+		{
+			val = x;
+		}
+	}
+}
