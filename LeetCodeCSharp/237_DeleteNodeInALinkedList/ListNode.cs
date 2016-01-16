b0de30ff3e01044be16625f810f59376885f2af1
morrieati <morrislew@outlook.com>
2016-01-16 21:14:53 +0800

    237. Delete Node in a Linked List

---
+++ LeetCodeCSharp/237_DeleteNodeInALinkedList/ListNode.cs
@@ -0,0 +1,12 @@
+﻿namespace _237_DeleteNodeInALinkedList
+{
+	public class ListNode
+	{
+		public int val;
+		public ListNode next;
+		public ListNode(int x)
+		{
+			val = x;
+		}
+	}
+}
---
+++ LeetCodeCSharp/237_DeleteNodeInALinkedList/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _237_DeleteNodeInALinkedList
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+
+		public void DeleteNode(ListNode node)
+		{
+			if (node.next != null)
+			{
+				node.val = node.next.val;
+				node.next = node.next.next;
+			}
+		}
+	}
+}
