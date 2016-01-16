25ea04f5c43e87e20507a666fc803949e96cca82
morrieati <morrislew@outlook.com>
2016-01-16 19:23:47 +0800

    258. Add Digits

---
+++ LeetCodeCSharp/258_AddDigits/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _258_AddDigits
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+
+		public int AddDigits(int num)
+		{
+			return (num - 1) % 9 + 1;
+		}
+	}
+}
+
+/*
+
+	1	1
+	2	2
+	3	3
+	4	4
+	5	5
+	6	6
+	7	7
+	8	8
+	9	9
+
+	10	1
+	11	2
+	12	3
+	13	4
+	14	5
+	15	6
+	16	7
+	17	8
+	18	9
+
+	19	1
+	20	2
+	21	3
+	22	4
+	23	5
+	24	6
+	25	7
+	26	8
+	27	9
+
+	28	1
+
+*/
