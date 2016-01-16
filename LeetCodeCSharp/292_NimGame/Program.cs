9405a0ac4211d2c92892da7a1d9757d93a58e04c
morrieati <morrislew@outlook.com>
2016-01-16 18:41:47 +0800

    292. Nim Game

---
+++ LeetCodeCSharp/292_NimGame/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _292_NimGame
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+		}
+
+		public bool CanWinNim(int n)
+		{
+			return !(n % 4 == 0);
+		}
+	}
+}
+
+/*
+
+	1	1
+	2	2
+	3	3
+	4	false
+	5	1
+	6	2
+	7	3
+	8	false
+
+*/
