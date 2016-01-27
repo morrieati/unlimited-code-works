37361d011d92b6f074fe465e152ee0ed2c6983ba
morrieati <morrislew@outlook.com>
2016-01-27 21:09:12 +0800

    169&235

--- LeetCodeCSharp/169_MajorityElement/Program.cs
+++ LeetCodeCSharp/169_MajorityElement/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,14 +11,42 @@ namespace _169_MajorityElement
 	{
 		static void Main(string[] args)
 		{
+			int[] nums = { 3, 2, 3 };
+			Console.WriteLine(MajorityElement(nums));
+			Console.ReadLine();
 		}
-
-		public int MajorityElement(int[] nums)
+		
+		public static int MajorityElement(int[] nums)
 		{
-			int majorityNum = 0;
+			int elem = 0;
+			int count = 1;
+			int length = nums.Length;
 
+			if (length == 1)
+			{
+				return nums[0];
+			}
+			
+			var numList = nums.ToList();
+			numList.Sort();
+			for (int i = 0; i < length - 1; i++)
+			{
+				if (numList[i] == numList[i + 1])
+				{
+					count++;
+					if (count > length / 2)
+					{
+						elem = numList[i];
+						break;
+					}
+				}
+				else
+				{
+					count = 1;
+				}
+			}
 
-			return majorityNum;
+			return elem;
 		}
 	}
 }
---
+++ LeetCodeCSharp/235_LowestCommonAncestor/TreeNode.cs
@@ -0,0 +1,13 @@
+﻿namespace _235_LowestCommonAncestor
+{
+	public class TreeNode
+	{
+		public int val;
+		public TreeNode left;
+		public TreeNode right;
+		public TreeNode(int x)
+		{
+			val = x;
+		}
+	}
+}
