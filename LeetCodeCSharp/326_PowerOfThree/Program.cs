a937434bdeb9a7ac4f2ea6187fc1681e56dc4742
morrieati <morrislew@outlook.com>
2016-01-30 20:21:53 +0800

    326. Power of Three

---
+++ LeetCodeCSharp/326_PowerOfThree/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _326_PowerOfThree
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(IsPowerOfThree(19682));
+		}
+
+		public static bool IsPowerOfThree(int n)
+		{
+			if (n == 0)
+			{
+				return false;
+			}
+			double dR = Math.Log(n, 3);
+			int iR = (int)Math.Round(dR);
+			double result = Math.Abs(dR - iR);
+			if (result < 0.0000000000001)
+			{
+				return true;
+			}
+			return false;
+		}
+	}
+}
