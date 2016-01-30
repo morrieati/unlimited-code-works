5cdb6a844672caf517e766513ec1b07ada1f879c
morrieati <morrislew@outlook.com>
2016-01-30 20:02:54 +0800

    231. Power of Two

---
+++ LeetCodeCSharp/231_PowerOfTwo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _231_PowerOfTwo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+
+		public bool IsPowerOfTwo(int n)
+		{
+			if (n == 0)
+			{
+				return false;
+			}
+			if (n == -2147483648)
+			{
+				return false;
+			}
+
+			if ((n & (n - 1)) == 0)
+			{
+				return true;
+			}
+			return false;
+		}
+	}
+}
