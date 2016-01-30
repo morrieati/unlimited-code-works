1bc10734eb7d20533930df96a61218cf4b122d15
morrieati <morrislew@outlook.com>
2016-01-30 19:43:24 +0800

    83. Remove Duplicates from Sorted List

---
+++ LeetCodeCSharp/83_RemoveDuplicates/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _83_RemoveDuplicates
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ListNode head = new ListNode(1);
+			head.next = new ListNode(1);
+			DeleteDuplicates(head);
+		}
+
+		public static ListNode DeleteDuplicates(ListNode head)
+		{
+			int temp = 0;
+			ListNode p = head;
+			while (p != null && p.next != null)
+			{
+				temp = p.val;
+				while (p.next.val == temp)
+				{
+					p.next = p.next.next;
+					if (p.next == null)
+					{
+						break;
+					}
+				}
+
+				p = p.next;
+			}
+
+			return head;
+		}
+	}
+}
